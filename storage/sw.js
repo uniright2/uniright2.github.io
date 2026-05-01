f2f75940fdc25be7acb9e24fc63ad71f54080421
uniright2 <uniright2@yahoo.co.jp>
2026-05-01 10:05:09 +0900

    Add files via upload

---
+++ storage/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'dq-rta-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// オフライン時にキャッシュから表示
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
